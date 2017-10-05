3f8ef8d7e4edd313c89d4e90aa9c1f9588006940
Alexis <alexis.alulema@gmail.com>
2017-10-05 05:04:43 -0500

    Finishing Joinery Example

--- src/com/alexisalulema/JsoupExample.java
+++ src/com/alexisalulema/JsoupExample.java
@@ -1,4 +1,49 @@
 package com.alexisalulema;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class JsoupExample {
+    public static void run() throws IOException {
+
+        InputStream fStream = Main.class.getResourceAsStream("asian-view.html");
+        InputStreamReader fReader = new InputStreamReader(fStream, StandardCharsets.UTF_8);
+        StringBuilder entireText = new StringBuilder();
+
+        try (Stream<String> lines = new BufferedReader(fReader).lines()) {
+            List<String> allLines = lines.collect(Collectors.toList());
+
+            for (String l: allLines) {
+                entireText.append(l);
+            }
+        }
+
+        fReader.close();
+        fStream.close();
+
+        String rawHtml = entireText.toString(); // UrlUtils.request("https://www.kaggle.com/c/avito-duplicate-ads-detection/leaderboard");
+        Document document = Jsoup.parse(rawHtml);
+        Elements tableRows = document.select("#mainTable tr");
+
+        for (Element tr : tableRows) {
+            Elements columns = tr.select("td");
+
+            if (columns.isEmpty())
+                continue;
+
+            String ob1 = columns.get(2).text();
+            System.out.println(ob1);
+        }
+    }
 }
--- src/com/alexisalulema/Main.java
+++ src/com/alexisalulema/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
         StreamsExample.run();
         CsvExample.run();
+        JsoupExample.run();
     }
 }
 
